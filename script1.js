4ec46b76c9e144790083e925650b38d645c258e9
lohar11 <loharp679@gmail.com>
2025-03-01 12:20:53 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDDbGzvOhRM6B2K_q_C1qSPPps4DRfUR5g",
+  authDomain: "skincare-product-website.firebaseapp.com",
+  databaseURL: "https://skincare-product-website-default-rtdb.firebaseio.com",
+  projectId: "skincare-product-website",
+  storageBucket: "skincare-product-website.firebasestorage.app",
+  messagingSenderId: "505826794216",
+  appId: "1:505826794216:web:badba508cfb902e8ba475d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
